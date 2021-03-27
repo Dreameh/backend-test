e185e270e533183ae7d13221c7e5a671be9991c7
Dreamo <kyusaki1@gmail.com>
2021-03-27 17:55:29 +0200

    fix cors now

--- src/main/java/dev/dreameh/backend/rest/config/CorsConfiguration.java
+++ src/main/java/dev/dreameh/backend/rest/config/CorsConfiguration.java
@@ -4,17 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 public class CorsConfiguration implements WebMvcConfigurer {
 
     // For local development
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+        var origins = List.of("https://frontend-testing-thesis.herokuapp.com");
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry
-                    .addMapping("http://localhost:8080")
-                    .allowedOrigins("http://localhost:8080");
+                    .addMapping(origins.get(0))
+                    .allowedOrigins(origins.get(0));
             }
         };
     }
